cd854df803e273a2e7a92d342e7a7789d83eb7e5
Adi Fine <adif@Adis-MacBook-Pro.local>
2019-08-26 14:14:58 +0300

    added function declaration to game.h

--- game.h
+++ game.h
@@ -1,1 +1,58 @@
+#ifndef GAME_H_   /* Include guard */
+#define GAME_H_
 
+enum mode{init, solve, edit}
+
+typedef struct board
+{
+    int n; /*n value*/
+    int m; /*m value*/
+    int N; /*not sure yet*/
+    
+    cell **cells;
+    
+    enum mode curr_mode
+    bool mark_errors
+
+} Board;
+
+typedef struct cell
+{
+    int value;
+    
+    bool fixed;
+    bool erroneous;
+    
+    
+} Cell;
+
+Board* createBoard(int col, row);
+
+/*
+ * check if there is colision of value with row and column
+ */
+bool isInRowAndColumn(Board* board, int val, int col, int row);
+
+/*
+ * check if there is colision of value with the block
+ */
+bool isInBlock(Board* board, int val, int col, int row);
+
+int getVal(Board* board, int col, int row);
+
+/*
+ * set value
+ */
+void setVal(Board* board, int col, int row, int value);
+
+/*
+ * set value if legal and return True is success
+ */
+bool trySetVal(Board* board, int col, int row, int value);
+
+/*
+ * This function checks whether the puzzle is solved and the game is over.
+ */
+bool isSolved(Board* board);
+
+#endif /* GAME_H_ */
